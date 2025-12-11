c3dd03c8ed4188574fe07fec5e35500686cf2bdc
jean-pierre-valarezo <jeanpierrevalarezovillalta.tmf@gmail.com>
2025-12-11 17:06:52 -0500

    arreglando algunas cosas

---
+++ src/app/shared/services/validation.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidationService {
+
+  static urlValidatorValue(value: string | undefined | null): boolean {
+    if (!value) return true; // opcional: no validar si está vacío
+    const pattern = /^(http:\/\/|https:\/\/).+/i;
+    return pattern.test(value.trim());
+  }
+
+
+  static onlyLettersValue(value: string | undefined | null): boolean {
+    if (!value) return false;
+    const regex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1 ]+$/;
+    return regex.test(value.trim());
+  }
+
+
+  static noNumbersValue(value: string | undefined | null): boolean {
+    if (!value) return false;
+    return !/\d/.test(value);
+  }
+
+
+  static minLengthValue(value: string | undefined | null, min: number): boolean {
+    if (!value) return false;
+    return value.trim().length >= min;
+  }
+}
