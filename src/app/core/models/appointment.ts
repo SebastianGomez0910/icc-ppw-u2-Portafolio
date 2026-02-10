f3589187783eee252bfbd2df96bfd5eb301786b1
Sebastian Gomez <sebgomez395@gmail.com>
2026-02-10 12:15:35 -0500

    Fix: endpoints apuntando a producción en Render

--- src/app/core/models/appointment.ts
+++ src/app/core/models/appointment.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../enviroments/environment';
 
 export interface AppointmentSlot {
   id: string;
@@ -19,7 +20,7 @@ export interface AppointmentSlot {
 export class AppointmentService {
 
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/api'; 
+  private apiUrl = `${environment.apiUrl}`;
 
   private getHeaders() {
     const token = localStorage.getItem('token'); 
